c7be7e64457ff7e46efea6884ba4b773d9e19d34
Sarah Norris <sarah@sekai.co.uk>
2024-12-09 19:54:23 +0000

    Install composer and lint PHP

--- src/render.php
+++ src/render.php
@@ -1,30 +1,32 @@
 <?php
 	// Get the block attributes.
-	$apiKey = isset( $attributes['apiKey'] ) ? esc_attr( $attributes['apiKey'] ) : '';
-	$username = isset( $attributes['username'] ) ? esc_attr( $attributes['username'] ) : '';
-	$numberOfTracks = isset( $attributes['numberOfTracks'] ) ? esc_attr( $attributes['numberOfTracks'] ) : '1';
-	$showTrackImage = isset( $attributes['showTrackImage'] ) ? $attributes['showTrackImage'] : false;
-	$includeLinkToTrack = isset( $attributes['includeLinkToTrack'] ) ? $attributes['includeLinkToTrack'] : false;
+	$api_key             = isset( $attributes['apiKey'] ) ? esc_attr( $attributes['apiKey'] ) : '';
+	$username           = isset( $attributes['username'] ) ? esc_attr( $attributes['username'] ) : '';
+	$number_of_tracks     = isset( $attributes['numberOfTracks'] ) ? esc_attr( $attributes['numberOfTracks'] ) : '1';
+	$show_track_image     = isset( $attributes['showTrackImage'] ) ? $attributes['showTrackImage'] : false;
+	$include_link_to_track = isset( $attributes['includeLinkToTrack'] ) ? $attributes['includeLinkToTrack'] : false;
 ?>
 
 <div <?php echo get_block_wrapper_attributes(); ?>>
 	<ul
 		class="tracks-list"
-		data-lastfm-apiKey="<?php echo esc_attr( $apiKey ); ?>"
+		data-lastfm-apiKey="<?php echo esc_attr( $api_key ); ?>"
 		data-lastfm-username="<?php echo esc_attr( $username ); ?>"
-		data-lastfm-numberOfTracks="<?php echo esc_attr( $numberOfTracks ); ?>"
-		<?php if ( $showTrackImage ) : ?>
+		data-lastfm-numberOfTracks="<?php echo esc_attr( $number_of_tracks ); ?>"
+		<?php if ( $show_track_image ) : ?>
 			data-lastfm-showTrackImage="true"
 		<?php endif; ?>
-		<?php if ( $includeLinkToTrack ) : ?>
+		<?php if ( $include_link_to_track ) : ?>
 			data-lastfm-includeLinkToTrack="true"
 		<?php endif; ?>
 	>
 		<li>
-			<?php echo __(
+			<?php
+			echo __(
 				'Recently played tracks loading…',
 				'lastfm-recently-played-block'
-				); ?>
+			);
+			?>
 		</li>
 	</ul>
 </div>
